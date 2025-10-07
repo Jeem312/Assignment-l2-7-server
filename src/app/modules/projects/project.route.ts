a548fc3450a791147497f51a62fc6fb229468f94
Jeem312 <shanjidajeem312@gmail.com>
2025-10-07 20:31:13 +0600

    added single project get route

--- src/app/modules/projects/project.route.ts
+++ src/app/modules/projects/project.route.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import { createProject, deleteProject, getAllProjects, updateProject } from "./project.controller";
+import { createProject, deleteProject, getAllProjects, singleProject, updateProject } from "./project.controller";
 import { checkAuth } from "../../../middleWares/checkAuth";
 
 const router = express.Router();
@@ -7,6 +7,7 @@ const router = express.Router();
 router.post("/", checkAuth("admin"), createProject);
 router.get("/", getAllProjects);
 router.patch("/:id", checkAuth("admin"), updateProject);
+router.get("/:id", singleProject);
 router.delete("/:id", checkAuth("admin"), deleteProject);
 
 export const ProjectRouter = router;
